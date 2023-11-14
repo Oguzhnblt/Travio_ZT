1ba6faa3a84eb6f05bcbcbfc900242a4e02e053e
Orhan Başpınar <orhanbspnr@gmail.com>
2023-11-14 12:08:22 +0300

    Security Settings validatefields control add

--- Travio_ZT/Travio/Controllers/SecuritySettingsPage/SecuritySettingsVM.swift
+++ Travio_ZT/Travio/Controllers/SecuritySettingsPage/SecuritySettingsVM.swift
@@ -1,27 +1,38 @@
-//
-//  SecuritySettingsVM.swift
-//  Travio
-//
-//  Created by web3406 on 13.11.2023.
-//
-
+// SecuritySettingsVM.swift
 import Foundation
 import Alamofire
 
 class SecuritySettingsVM {
-    func changePassword(profile: ChangePasswordRequest) {
-        
-        
-        let params = ["new_password": profile.new_password]
-        
-        NetworkingHelper.shared.fetchData(urlRequest: .changePassword(params: params as Parameters)) {(result: Result<ChangePasswordResponse, Error>) in
-            switch result {
-                case .success(let success):
-                    print(success.message!)
-                case .failure(let err):
-                    print(err.localizedDescription)
+    func validatePasswordFields(newPassword: String?, confirmPassword: String?) -> ValidationResult {
+        guard let new_password = newPassword, !new_password.isEmpty,
+              let new_password_confirm = confirmPassword, !new_password_confirm.isEmpty
+        else {
+            return .failure("Lütfen tüm alanları doldurun.")
+        }
+
+        if new_password.count >= 6 {
+            if new_password == new_password_confirm {
+                return .success
+            } else {
+                return .failure("Şifreler eşleşmiyor.")
             }
+        } else {
+            return .failure("Şifre en az 6 karakter içermelidir.")
         }
     }
-    
+
+    func changePassword(profile: ChangePasswordRequest, completion: @escaping (Result<ChangePasswordResponse, Error>) -> Void) {
+        let params = ["new_password": profile.new_password]
+
+        NetworkingHelper.shared.fetchData(urlRequest: .changePassword(params: params as Parameters)) { result in
+            completion(result)
+        }
+    }
 }
+
+enum ValidationResult {
+    case success
+    case failure(String)
+}
+
+
